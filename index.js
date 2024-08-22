f77a6e39e135b024d3289a70d4280854d4dd6d09
Kientega <Kienteganourdine72@gamil.com>
2024-08-22 18:50:05 +0000

    Adding my name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello world !');
+    res.end('Hello world My name is Nourdine !');
 });
 
 const port = process.env.PORT || 3000;
