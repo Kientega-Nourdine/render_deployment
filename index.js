0406daef0da37d0e1575f7b1355071c0855001c3
Kientega <Kienteganourdine72@gamil.com>
2024-08-22 18:28:18 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// require http module 
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world !');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => console.log(`App running port http://localhost:${port}`));
